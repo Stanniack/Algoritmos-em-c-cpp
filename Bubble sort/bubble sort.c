4e8d0d02302c17c846631154a7bad67822c47d52
Mateus Vitor <44790156+Stanniack@users.noreply.github.com>
2021-03-20 00:45:48 -0300

    Bubble sort recursivo

--- Bubble sort/bubble sort.c
+++ Bubble sort/bubble sort.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
 void bubbleSort(int array[], int len);
+int recursiveBubbleSort(int array[], int len);
+
 #define DEBUG
 #undef DEBUG
 
@@ -9,7 +11,7 @@ int main () {
 
     int array[] = {4, 7, 2, 1, 5}, len, i;
     len = sizeof(array)/sizeof(array[0]);
-    bubbleSort(array, len - 1);
+    recursiveBubbleSort(array, len - 1);
 
     for (int i = 0; i < len; ++i) {
         printf("%d ", array[i]);
@@ -25,9 +27,9 @@ void bubbleSort (int array[], int len) {
     while (len > 0) {
 
         for (i = 0; i < len; i++) {
-            #ifdef DEBUG
-                printf("%d ? %d\n", array[i], array[i + 1]);
-            #endif
+#ifdef DEBUG
+            printf("%d ? %d\n", array[i], array[i + 1]);
+#endif
 
             if (array[i] > array[i + 1]) {
                 int tmp = array[i + 1];
@@ -38,5 +40,23 @@ void bubbleSort (int array[], int len) {
 
         len--;
     }
+}
+
+int recursiveBubbleSort (int array[], int len) {
+    int i;
+
+    if (len < 0)
+        return 1;
+
+    for (i = 0; i < len; i++) {
+
+        if (array[i] > array[i + 1]) {
+            int tmp = array[i + 1];
+            array[i + 1] = array[i];
+            array[i] = tmp;
+        }
+    }
+
+    recursiveBubbleSort(array, len - 1);
 }
 
