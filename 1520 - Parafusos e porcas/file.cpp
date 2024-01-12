1c875c29ca5440525fa6ae9ea0b98fdf093c3988
Mateus Vitor <stanniack@gmail.com>
2024-01-12 17:15:38 -0300

    Código ainda continua ineficiente igual busca linear
    O chat gpt fez a busca binária editada /o\

--- 1520 - Parafusos e porcas/file.cpp
+++ 1520 - Parafusos e porcas/file.cpp
@@ -1,17 +1,91 @@
 #include <iostream>
-#include <stack>
+#include <vector>
+#include <bits/stdc++.h>
 
 using namespace std;
 
+vector<int> findAllOccurrences(int key, const std::vector<int>& array);
+
 main()
 {
+    int n, x;
+    vector<int> vec;
+
+    while (!(cin >> n).eof())
+    {
+        for (int i = 0; i < n; i++)
+        {
+            int _begin, _end;
+            cin >> _begin >> _end;
+
+            int tam = 0;
+
+            if (_end >= _begin)
+            {
+                while (_begin <= _end)
+                {
+                    vec.push_back(_begin);
+                    _begin++;
+
+                }
+            }
+        }
+
+        cin >> x;
+
+        sort(vec.begin(), vec.end());
+
+        vector <int> occurrences= findAllOccurrences(x, vec);
+
+        //todo
+
+        if (!occurrences.empty()) cout << x << " found from " << occurrences.front() << " to " << occurrences.back() << endl;
+        else cout << x << " not found" << endl;
+    }
 
 }
 
+/* Código adaptado pelo Chat GPT */
+vector<int> findAllOccurrences(int key, const std::vector<int>& array) {
+    std::vector<int> occurrences;
+
+    int low = 0;
+    int high = array.size() - 1;
+
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+
+        if (array[mid] == key) {
+            occurrences.push_back(mid);
+
+            // Encontrar mais ocorrências à esquerda
+            int left = mid - 1;
+            while (left >= 0 && array[left] == key) {
+                occurrences.push_back(left);
+                --left;
+            }
+
+            // Encontrar mais ocorrências à direita
+            int right = mid + 1;
+            while (right < array.size() && array[right] == key) {
+                occurrences.push_back(right);
+                ++right;
+            }
+
+            break;  // Importante para evitar repetições desnecessárias no vetor
+        } else if (array[mid] < key) {
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
+    }
+
+    return occurrences;
+}
 
 
 /*
-_______________________________
+______________________________
 3
 
 1 - (1, 3)
